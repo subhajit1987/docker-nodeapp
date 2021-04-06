5591845bdb3e8d387fca1e9954553d1237ab9d7a
Subhajit Chakraborty <subhaawsacc@gmail.com>
2021-04-06 20:48:16 +0530

    modified app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there! Welcome to my website friends!
+          Hello there! Welcome to my website friends! This was commited to dev branch.
         </p>
         <a
           className="App-link"
